e9e0864c00f82a49feef13de45533a55821981d4
dumpling0706 <olysenko2013@mail.ru>
2018-06-29 15:44:51 +0900

    second

--- main.cpp
+++ main.cpp
@@ -10,19 +10,49 @@ void view(const Complex &c)
 
 
 void main() {
-	Complex a(15,2);
-	Complex b(3, 5);
-   view(a);
-   view(b);
+vector <Complex>o;
+
+	
+	Complex c;
+	Complex r;
+	for (int i = 0; i < 10; i++)
+	
+		o.push_back(Complex(i,i));
+	r = o[2];
+	view(r);//вывод эл вект
+	o[2] = Complex(1, 4);//изменение эл вект
+	view(o[2]);
+	Complex a(1,2);
+	Complex b(1, 2);
+	float g;
+	g = a.Re() + a.Im();
+	view(a);
+	view(b);
+	(a + b).abs();
+	cout <<(a+b).abs() << endl;
+	
+	c = a+b;
+	view(c);
+
+	
+	for (int i = 0; i < 10; i++)
+		view(o[i]);
    view(a + b);
-   view(a - b);
-   view(a + 5);
+   //view(a - b);
+   //view(a + 5);
    //view(a / b);
- /*  view(a*b);
+  view(a*b);
    view(a * 5);
    view(a / 5);
-   cout << a.abs();*/
+
+
+   cout << c.abs()<<endl;
+
+   cout << g;
+
    
+
+	
 
 	system("pause");
 }
